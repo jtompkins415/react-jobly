61af25285fe1c8df548a130ea7345a0beaaff201
jtompkins415 <john.tompkins415@gmail.com>
2023-02-01 10:25:22 -0800

    Create CompanyCard component

--- src/CompanyCard.js
+++ src/CompanyCard.js
@@ -1,16 +1,31 @@
-import axios from 'axios';
-import JoblyApi from './api';
+import {
+    Card,
+    CardBody,
+    CardTitle,
+    CardText
+} from 'reactstrap'
 
-const CompanyCard = () => {
-     const getCompany = async () => { 
-        const res = await JoblyApi.request('companies')
-
-        console.log(res);
-     }
-
+const CompanyCard = (name, numEmployees) => {
+     
     return (
         <>
-            <button onClick={getCompany}>Click me </button>
+         <Card
+            body
+            outline
+            color='primary'
+            style={{
+                width: '10rem'
+            }} 
+         >
+            <CardBody>
+                <CardTitle>
+                    {name}
+                </CardTitle>
+                <CardText>
+                    {numEmployees}
+                </CardText>
+            </CardBody>
+         </Card>   
         </>
     )
 };
