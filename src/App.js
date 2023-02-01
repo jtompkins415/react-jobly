4aa05210c0083357d7a115bd8dbbf5278a0a46fc
jtompkins415 <john.tompkins415@gmail.com>
2023-01-31 18:56:17 -0800

    import components, write routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
+
+import NavBar from './NavBar';
+import Home from './Home';
+import CompanyList from './CompanyList';
+import CompanyDetails from './CompanyDetails';
+import JobList from './JobList';
+import LoginForm from './LoginForm';
+import SignupForm from './SignupForm';
+import UserDetails from './UserDetails';
+import CompanyCard from './CompanyCard';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
+      <header>
+        <NavBar />
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/companies' element={<CompanyList />} />
+          <Route path='/companies/:handle' element={<CompanyDetails />} />
+          <Route path='/jobs' element={<JobList />} />
+          <Route path='/users/login' element={<LoginForm />} />
+          <Route path='/users/signup' element={<SignupForm />} />
+          <Route path='/users/profile' element= {<UserDetails />} />
+        </Routes>
+      </BrowserRouter>
+      <footer>
+        <CompanyCard />
+      </footer>
     </div>
   );
 }
