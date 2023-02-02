20aa09cb65c2c09d5507e80772fca1d9d51e266f
jtompkins415 <john.tompkins415@gmail.com>
2023-02-01 17:19:49 -0800

    import SearchForm component, import CardGroup

--- src/CompanyList.js
+++ src/CompanyList.js
@@ -1,36 +1,61 @@
 import {useState, useEffect} from 'react';
+import { CardGroup } from 'reactstrap';
 import JoblyApi from './api'
 import CompanyCard from './CompanyCard';
+import SearchForm from './SearchForm';
+
 
 const CompanyList = () => {
+
+    //Set state for companies data
+
     const [compData, setCompData] = useState([]);
 
-    useEffect(() => {
+    //Grab companies data
+
+     useEffect(() => {
         async function getCompanies(){
         const companies = await JoblyApi.request('companies')
         setCompData(companies); 
         } getCompanies()
     }, [])
 
-    
+    //search function that sets the state with the data found
+   
+    //If no company data return message
 
+    if (!compData.companies) return <div><h1>No companies here!</h1></div>
+    
+    //Push companies data to array
     let comps = [];
-    for (let i = 0; i < compData.companies.length; i++){
+    for(let i = 0; i < compData.companies.length; i++){
         comps.push(compData.companies[i])
     }
 
-    let compElm = comps.map((c) => {
-        return (
-            <CompanyCard key={c.handle} name={c.name} numEmployees={c.numEmployees} />
+    //Map over the companies data array and create components based on the data
+
+    let compElm = comps.map(c => {
+        return ( 
+            <CompanyCard key={c.handle} handle={c.handle} name={c.name} description={c.description} logoUrl={c.logoUrl} />
         )
     })
 
+    
+
+
     return (
-        <div className="CompanyList">
+        <div className='CompanyList'>
+            <header className='CompanyList-header'>
             <h1>COMPANIES</h1>
-            <ul>
+            </header>
+            <div className='search-bar'>
+            <SearchForm />
+            </div>
+            <div className='C ompanyList-list'>
+                <CardGroup>
                 {compElm}
-            </ul>
+                </CardGroup>
+            </div>
         </div>
 
     )
