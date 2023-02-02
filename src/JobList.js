4f9a2311162a86460192907af6c934d16b3a230a
jtompkins415 <john.tompkins415@gmail.com>
2023-02-01 20:09:55 -0800

    write out functionality for JobList component

--- src/JobList.js
+++ src/JobList.js
@@ -1,8 +1,55 @@
+import {useState, useEffect} from 'react';
+import { CardGroup } from 'reactstrap';
+import JoblyApi from './api';
+import JobCard from './JobCard';
+
 
 const JobList = () => {
+    const [jobData, setJobData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true)
+
+    //Grab jobs data
+
+     useEffect(() => {
+        async function getJobs(){
+        const jobs = await JoblyApi.request('jobs')
+        setJobData(jobs); 
+        setIsLoading(false)
+        } getJobs()
+    }, [])
+
+    //search function that sets the state with the data found
+   
+
+    //Let user know if page is loading, if no companies show user
+    if (isLoading) return <div><h1>Loading...</h1></div>
+    if (!jobData.jobs) return <div><h1>No jobs here!</h1></div>
+    
+    //Push companies data to array
+    let jobs = [];
+    for(let i = 0; i < jobData.jobs.length; i++){
+        jobs.push(jobData.jobs[i])
+    }
+
+    //Map over the companies data array and create components based on the data
+
+    let jobElm = jobs.map(j => {
+        return ( 
+            <JobCard key={j.id} id={j.id} title={j.title} salary={j.salary} equity={j.equity} company_handle={j.company_handle} />
+        )
+    })
+
+    
     return (
         <div className="JobList">
-            <h1>JOBS</h1>
+            <header>
+                <h1>JOBS</h1>
+            </header>
+            <div className='JobList-list'>
+                <CardGroup>
+                {jobElm}
+                </CardGroup>
+            </div>
         </div>
     )
 };
