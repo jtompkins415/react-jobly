235166af2b6da1a89c2d719bade0b6264fb996ad
jtompkins415 <john.tompkins415@gmail.com>
2023-02-06 10:18:29 -0800

    Organize Components into files

---
+++ src/companies/CompanyDetails.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom'
+import JoblyApi from "../api";
+
+const CompanyDetails = () => {
+
+    const { handle } = useParams()
+
+    const [compData, setCompData] = useState(null)
+    const [isLoading, setIsLoading] = useState(true);
+    
+   useEffect(() => {
+    async function getCompany(){
+        setCompData(await JoblyApi.getCompany(handle))
+        setIsLoading(false)
+    } 
+    getCompany ()
+   }, [handle])
+
+   if (isLoading) return <div><h1>Loading...</h1></div>
+
+    return (
+        <div>
+            <h4>{compData.name}</h4>
+            <h6>{compData.description}</h6>
+        </div>
+    )
+};
+
+export default CompanyDetails;
+
---
+++ src/companies/CompanyList.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import { CardGroup } from 'reactstrap';
+import JoblyApi from '../api'
+import CompanyCard from './CompanyCard';
+import SearchForm from '../SearchForm';
+
+
+const CompanyList = () => {
+
+    //Set state for companies data
+
+    const [compData, setCompData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true)
+
+    //use search function to grab companies data
+
+     useEffect(() => {
+        searchFor();
+    }, [])
+
+    //search function that sets the state with the data found
+
+    const searchFor = async (value) => {
+        const res = await JoblyApi.getCompanies(value);
+        setCompData(res)
+        setIsLoading(false);
+    }
+   
+    //Let user know if page is loading, if no companies show user
+    if (isLoading) return <div><h1>Loading...</h1></div>
+    if(!compData.length) return <div><h1>Sorry... No Companies</h1></div>
+    
+    // //Push companies data to array
+    let comps = [];
+    for(let i = 0; i < compData.length; i++){
+        comps.push(compData[i])
+    }
+
+    // //Map over the companies data array and create components based on the data
+
+    let compElm = comps.map(c => {
+        return ( 
+            <CompanyCard key={c.handle} handle={c.handle} name={c.name} description={c.description} logoUrl={c.logoUrl} />
+        )
+    })
+
+
+    return (
+        <div className='CompanyList'>
+            <header className='CompanyList-header'>
+            <h1>COMPANIES</h1>
+            </header>
+            <div className='search-bar'>
+            <SearchForm searchFor={searchFor}/>
+            </div>
+            <div className='CompanyList'>
+                <CardGroup>
+                {compElm}
+                </CardGroup>
+            </div>
+        </div>
+    );
+};
+
+export default CompanyList;
